14311e7daf0316f09dd7a12797d1ccda5619dcd8
cj86pierce <cj86pierce@gmail.com>
2026-03-15 12:51:35 -0400

    Deploy

--- src/lib/avatarConfigFromUser.ts
+++ src/lib/avatarConfigFromUser.ts
@@ -11,12 +11,19 @@ type UserAvatarFields = {
   mouthStyle: string;
   shirtStyle: string;
   accessoryStyle: string;
+  glassesStyle?: string;
+  scarStyle?: string;
+  hairOrnamentStyle?: string;
   bodyColor: string;
   hairColor: string;
   eyeColor: string;
   mouthColor: string;
   shirtColor: string;
   accessoryColor: string;
+  backgroundColor?: string;
+  glassesColor?: string;
+  scarColor?: string;
+  hairOrnamentColor?: string;
 };
 
 export function avatarConfigFromUser(user: UserAvatarFields): AvatarConfig {
@@ -27,11 +34,18 @@ export function avatarConfigFromUser(user: UserAvatarFields): AvatarConfig {
     mouthStyle: oneOf(user.mouthStyle, ["mouth_01", "mouth_02", "mouth_03", "mouth_04", "mouth_05", "mouth_06"], "mouth_01"),
     shirtStyle: oneOf(user.shirtStyle, ["shirt_01", "shirt_02", "shirt_03", "shirt_04", "shirt_05", "shirt_06"], "shirt_01"),
     accessoryStyle: oneOf(user.accessoryStyle, ["none", "accessory_01"], "none"),
+    glassesStyle: oneOf(user.glassesStyle ?? "none", ["none"], "none"),
+    scarStyle: oneOf(user.scarStyle ?? "none", ["none"], "none"),
+    hairOrnamentStyle: oneOf(user.hairOrnamentStyle ?? "none", ["none"], "none"),
     bodyColor: user.bodyColor,
     hairColor: user.hairColor,
     eyeColor: user.eyeColor,
     mouthColor: user.mouthColor,
     shirtColor: user.shirtColor,
     accessoryColor: user.accessoryColor,
+    backgroundColor: user.backgroundColor ?? "#E8E8E8",
+    glassesColor: user.glassesColor ?? "#111111",
+    scarColor: user.scarColor ?? "#8B4513",
+    hairOrnamentColor: user.hairOrnamentColor ?? "#C0C0C0",
   };
 }
